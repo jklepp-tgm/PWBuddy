6bf12154ae2a9911f290f60605f1dddf9a6b216f
Jakob Klepp <jakob@truh.in>
2013-05-14 14:13:46 +0200

    Ähm ...

---
+++ src/main/java/com/pwbuddy/PBCategorieView.java
@@ -0,0 +1,14 @@
+package com.pwbuddy;
+
+import javax.swing.*;
+
+/**
+ * @author jakob
+ * @version 2013-05-14
+ */
+public class PBCategorieView extends JPanel {
+    private PBCategorie categorie;
+    public PBCategorieView(PBCategorie categorie) {
+        //To change body of created methods use File | Settings | File Templates.
+    }
+}
--- src/main/java/com/pwbuddy/PBView.java
+++ src/main/java/com/pwbuddy/PBView.java
@@ -1,15 +1,15 @@
 package com.pwbuddy;
 
 import javax.swing.*;
+import java.util.ArrayList;
 
 /**
  * @author jakob
  * @version 2013-04-09
  */
-public class PBView extends JScrollPane {
-    private PBTree tree;
+public class PBView extends JPanel {
+    private JScrollBar scrollBar;
+    private ArrayList<PBCategorie> categories;
     public PBView(){
-        this.tree = new PBTree();
-        this.add(this.tree);
     }
 }
