37de4c35c9b869dc80c5cca7ad09289e511a1bbe
Jakob Klepp <jakob@truh.in>
2013-05-28 16:31:09 +0200

    Unnötige main Methode gelöscht

--- src/main/java/com/pwbuddy/PBEncryptionCore.java
+++ src/main/java/com/pwbuddy/PBEncryptionCore.java
@@ -62,69 +62,7 @@ public class PBEncryptionCore {
         return new String(decryptedByte);
     }
 
-    public String getIV()
-    {
+    public String getIV() {
         return new BASE64Encoder().encode(this.cipher.getIV());
-    }
-    public static void main(String[] args)
-    {
-        /*
-        String pkey = "7ca8ed779908923a093a5843e5dacc2a4fc32f19c09d4ca53a6f6b83ad";
-
-        try
-        {
-            // Privater Schlüssel in Byte-Array repräsentation
-            byte[] key = pkey.getBytes("UTF-8");
-
-            MessageDigest sha = MessageDigest.getInstance("SHA-1");
-
-            // Schlüssel mit SHA1 Hashen
-            key = sha.digest(key);
-            // Erste 16 Bytes (128 Bit) kopieren, um immer dieselbe Länge zu haben
-            key = Arrays.copyOf(key, 16);
-
-            SecretKeySpec secret = new SecretKeySpec(key, "AES");
-
-            Cipher secure = Cipher.getInstance("AES/CBC/PKCS5Padding");
-
-            // Verschlüsselungs-Objekt mit geheimen Schlüssel initialisieren
-            secure.init(Cipher.ENCRYPT_MODE, secret);
-
-            // Verschlüsselung durchführen
-            byte[] encrypted = secure.doFinal("Verschlüsselter Text".getBytes("UTF-8"));
-
-            // Initialisierungs Vektor
-            byte[] iv = secure.getIV();
-
-            // Base64 repräsentation des verschlüsselten Passwortes
-            String encPW = new BASE64Encoder().encode(encrypted);
-
-            // Base64 repräsentation des IV
-            String encIV = new BASE64Encoder().encode(iv);
-
-            // *** DEBUG ***
-            System.out.println("Encrypted String (byte[]): "+new String(encrypted));
-            System.out.println("Encrypted String (Base64): "+encPW);
-
-            RandomAccessFile file = new RandomAccessFile("test.txt", "rw");
-            file.writeBytes(encPW);
-            file.close();
-
-            System.out.println("DECRYPTING:\n");
-
-            // Entschlüsselung initialisieren mit Schlüssel & IV
-            secure.init(Cipher.DECRYPT_MODE, secret, new IvParameterSpec(new BASE64Decoder().decodeBuffer(encIV)));
-
-            // Entschlüsseln
-            byte[] original = secure.doFinal(new BASE64Decoder().decodeBuffer(encPW));
-
-            String originalString = new String(original);
-            System.out.println("Decrypted String: " + originalString);
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
-        */
     }
 }
