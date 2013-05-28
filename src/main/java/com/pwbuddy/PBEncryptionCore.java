adbb269f9c43b54cce17444dcca625c3ed525319
andreas <awi95@gmx.at>
2013-05-28 16:24:32 +0200

    Encryption Core aktualisiert

--- src/main/java/com/pwbuddy/PBEncryptionCore.java
+++ src/main/java/com/pwbuddy/PBEncryptionCore.java
@@ -5,12 +5,9 @@ import sun.misc.BASE64Encoder;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
-import java.io.File;
 import java.io.IOException;
-import java.io.RandomAccessFile;
 import java.io.UnsupportedEncodingException;
 import java.security.*;
 import java.util.Arrays;
@@ -23,8 +20,55 @@ import javax.crypto.Cipher;
  * @since 2013-05-28
  */
 public class PBEncryptionCore {
+    private SecretKeySpec key;
+    private Cipher cipher;
+
+    public PBEncryptionCore(String pkey)
+    {
+        try
+        {
+            byte[] tKey = pkey.getBytes("UTF-8");
+
+            MessageDigest sha = MessageDigest.getInstance("SHA-1");
+
+            // Schlüssel mit SHA1 Hashen
+            tKey = sha.digest(tKey);
+            // Erste 16 Bytes (128 Bit) kopieren, um immer dieselbe Länge zu haben
+            tKey = Arrays.copyOf(tKey, 16);
+
+            key = new SecretKeySpec(tKey, "AES");
+
+            this.cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public String encrypt(String input ) throws InvalidKeyException, UnsupportedEncodingException, BadPaddingException, IllegalBlockSizeException {
+        this.cipher.init(Cipher.ENCRYPT_MODE, this.key);
+
+        byte[] encryptedByte = this.cipher.doFinal(input.getBytes("UTF-8"));
+
+        return new BASE64Encoder().encode(encryptedByte);
+    }
+
+    public String decrypt(String input, String iv) throws IOException, InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+        this.cipher.init(Cipher.DECRYPT_MODE, this.key, new IvParameterSpec(new BASE64Decoder().decodeBuffer(iv)));
+
+        byte[] decryptedByte = this.cipher.doFinal(new BASE64Decoder().decodeBuffer(input));
+
+        return new String(decryptedByte);
+    }
+
+    public String getIV()
+    {
+        return new BASE64Encoder().encode(this.cipher.getIV());
+    }
     public static void main(String[] args)
     {
+        /*
         String pkey = "7ca8ed779908923a093a5843e5dacc2a4fc32f19c09d4ca53a6f6b83ad";
 
         try
@@ -81,5 +125,6 @@ public class PBEncryptionCore {
         {
             e.printStackTrace();
         }
+        */
     }
 }
