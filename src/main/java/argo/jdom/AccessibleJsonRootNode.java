e1fc1770c3813674efbefb8f1d4b53b7b6b2eacf
Jakob Klepp <jakob@truh.in>
2013-05-31 19:56:47 +0200

    AccessibleJsonNode und AccessibleJsonRootNode erstellt
    Im gegensatz zu ihren Eltern haben die beiden public Konstruktoren.
    Die Eltern Klassen haben package private Kostruktoren um das vererben
    und Objekterzeugen aus anderen Packages zu verhindern.

---
+++ src/main/java/argo/jdom/AccessibleJsonRootNode.java
@@ -0,0 +1,12 @@
+package argo.jdom;
+
+/**
+ * Der Konstruktor von JsonRootNode ist Package-Private
+ * Mit dieser Klasse wird dies umgangen.
+ *
+ * @author Jakob Klepp
+ * @since 2013-05-31
+ */
+public abstract class AccessibleJsonRootNode extends JsonRootNode {
+    public AccessibleJsonRootNode(){}
+}
