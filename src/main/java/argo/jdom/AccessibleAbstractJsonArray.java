06927fad7676ffadf6364605d903951260bcf881
Jakob Klepp <jakob@truh.in>
2013-06-01 23:07:45 +0200

    class javadoc dem Projektstandard angepasst

--- src/main/java/argo/jdom/AccessibleAbstractJsonArray.java
+++ src/main/java/argo/jdom/AccessibleAbstractJsonArray.java
@@ -1,8 +1,8 @@
 package argo.jdom;
 
 /**
- * @author jakob
- * @version 2013-06-01
+ * @author Jakob Klepp
+ * @since 2013-06-01
  */
 public abstract class AccessibleAbstractJsonArray extends AbstractJsonArray {
     public AccessibleAbstractJsonArray(){}
