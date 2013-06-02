6ba5d7fbcc002f3a108b8ae92dacaac74fd08c75
Jakob Klepp <jakob@truh.in>
2013-06-02 19:27:54 +0200

    Soll helfen DataSet zu serialisieren

---
+++ src/main/java/com/pwbuddy/DataSetJsonNode.java
@@ -0,0 +1,41 @@
+package com.pwbuddy;
+
+import argo.jdom.AccessibleAbstractJsonObject;
+import argo.jdom.JsonField;
+import argo.jdom.JsonNode;
+import argo.jdom.JsonStringNode;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Jakob Klep
+ * @since 2013-06-02
+ */
+public class DataSetJsonNode extends AccessibleAbstractJsonObject {
+    /**
+     * Gets the fields associated with this node as a map of name to value.  Note that JSON permits
+     * duplicated keys in an object, though in practice this is rare, and in this case, this method
+     * will return a map containing a single entry of each unique key.
+     *
+     * @return the fields associated with this node
+     * @throws IllegalStateException if hasFields() returns false, indicating this type of node doesn't support fields.
+     */
+    @Override
+    public Map<JsonStringNode, JsonNode> getFields() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    /**
+     * Gets the fields associated with this node as a list of {@code JsonFields}.  This method allows
+     * the retrieval of all fields in an object even when the fields have duplicate keys.  This method
+     * also preserves the order of the fields.
+     *
+     * @return the fields associated with this node
+     * @throws IllegalStateException if hasFields() returns false, indicating this type of node doesn't support fields.
+     */
+    @Override
+    public List<JsonField> getFieldList() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
