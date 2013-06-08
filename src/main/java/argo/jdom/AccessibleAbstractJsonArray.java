850d4bedc230192e48accf9bb703375795fa00ea
Jakob Klepp <jakob@truh.in>
2013-06-08 18:24:21 +0200

    AccessibleAbstractJsonArray entfernt da es vermutlich nie verwendung finden wird

--- src/main/java/argo/jdom/AccessibleAbstractJsonArray.java
+++
@@ -1,9 +0,0 @@
-package argo.jdom;
-
-/**
- * @author Jakob Klepp
- * @since 2013-06-01
- */
-public abstract class AccessibleAbstractJsonArray extends AbstractJsonArray {
-    public AccessibleAbstractJsonArray(){}
-}
